167364779a90d03e12bd742a4d9384dca70f2df3
Andrzej Zwierzchowski <azwierzchowski@gmail.com>
2019-10-21 23:34:19 +0100

    Multi-root workspace support changes

--- src/langserver/toolsProjectSymbolsRequest.ts
+++ src/langserver/toolsProjectSymbolsRequest.ts
@@ -2,11 +2,13 @@ export class ToolsProjectSymbolsRequest {
     includeDependencies : boolean;
     projectPath : string;
     packagesFolder : string;
+    workspaceFolders : string[];
 
-    constructor(newIncludeDependencies : boolean, newProjectPath : string, newPackagesFolder : string) {
+    constructor(newIncludeDependencies : boolean, newProjectPath : string, newPackagesFolder : string, newWorkspaceFolders : string[]) {
         this.includeDependencies = newIncludeDependencies
         this.projectPath = newProjectPath;
         this.packagesFolder = newPackagesFolder;
+        this.workspaceFolders = newWorkspaceFolders;
     }
 
 }
