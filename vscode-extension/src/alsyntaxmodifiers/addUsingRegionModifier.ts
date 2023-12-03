1157a004a9194ebac8ad9ef05b9d4ac29b6e7b99
Andrzej Zwierzchowski <azwierzchowski@gmail.com>
2023-12-03 13:50:15 +0000

    Idea #509 - Add region around namespaces

--- vscode-extension/src/alsyntaxmodifiers/addUsingRegionModifier.ts
+++ vscode-extension/src/alsyntaxmodifiers/addUsingRegionModifier.ts
@@ -1,3 +1,4 @@
+import * as vscode from 'vscode';
 import { DevToolsExtensionContext } from "../devToolsExtensionContext";
 import { WorkspaceCommandSyntaxModifier } from "./workspaceCommandSyntaxModifier";
 
@@ -5,6 +6,13 @@ export class AddUsingRegionModifier extends WorkspaceCommandSyntaxModifier {
 
     constructor(context: DevToolsExtensionContext) {
         super(context, "Add Using Region", "addUsingRegion");
+    }
+
+    protected getParameters(uri: vscode.Uri): any {
+        let parameters = super.getParameters(uri);
+        let settings = vscode.workspace.getConfiguration('alOutline', uri);                
+        parameters.title = settings.get<string>('usingsRegionTitle');
+        return parameters;
     }
 
 }
